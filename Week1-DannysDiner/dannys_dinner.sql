de75d8e82c1e1271ef9bc3a83909f8b8ba383152
WAKIOM <agnessmanga@outlook.com>
2023-07-08 15:04:48 +0300

    finish writing the queries

--- Week1-DannysDiner/dannys_dinner.sql
+++ Week1-DannysDiner/dannys_dinner.sql
@@ -15,9 +15,9 @@ ORDER BY days_visited DESC;
 -- What was the first item from the menu purchased by each customer?
 SELECT DISTINCT s.customer_id, m.product_name AS first_buy
 FROM (
-		  SELECT customer_id, MIN(order_date) AS min_order_date
-		  FROM sales
-		  GROUP BY customer_id
+	SELECT customer_id, MIN(order_date) AS min_order_date
+	FROM sales
+	GROUP BY customer_id
 ) AS s
 JOIN sales AS s2 ON s.customer_id = s2.customer_id AND s.min_order_date = s2.order_date
 JOIN menu AS m ON s2.product_id = m.product_id
@@ -29,11 +29,11 @@ FROM sales AS s
 INNER JOIN menu AS m
     USING(product_id)
 WHERE s.product_id IN (
-		    SELECT product_id
-		    FROM sales
-		    GROUP BY product_id
-		    ORDER BY COUNT(*) DESC
-		    LIMIT 1
+	SELECT product_id
+	FROM sales
+	GROUP BY product_id
+	ORDER BY COUNT(*) DESC
+	LIMIT 1
 )
 GROUP BY s.customer_id, m.product_name
 ORDER BY product_count DESC;
@@ -43,7 +43,7 @@ SELECT customer_id, product_name AS most_popular_product, purchase_count
 FROM (
 	SELECT customer_id, product_name, COUNT(*) AS purchase_count,
 	RANK() OVER (PARTITION BY customer_id
-				ORDER BY COUNT(*) DESC) AS rank
+	ORDER BY COUNT(*) DESC) AS rank
 	FROM sales
 	INNER JOIN menu USING(product_id)
 	GROUP BY customer_id, product_name
@@ -55,8 +55,8 @@ ORDER BY customer_id;
 SELECT subquery.customer_id, m.product_name AS first_purchase_as_member
 FROM (
 	SELECT s.customer_id, s.product_id, s.order_date,
-		 ROW_NUMBER() OVER (PARTITION BY s.customer_id 
-	     ORDER BY s.order_date) AS rn
+	ROW_NUMBER() OVER (PARTITION BY s.customer_id 
+	   ORDER BY s.order_date) AS rn
 	FROM sales AS s
 	INNER JOIN members AS mem 
 	ON s.customer_id = mem.customer_id AND s.order_date >= mem.join_date
@@ -68,8 +68,8 @@ WHERE subquery.rn = 1;
 SELECT subquery.customer_id, m.product_name AS last_purchase_before_join
 FROM (
   SELECT s.customer_id, s.product_id, s.order_date,
-        RANK() OVER (PARTITION BY s.customer_id 
-		ORDER BY s.order_date DESC) AS rnk
+  RANK() OVER (PARTITION BY s.customer_id 
+  ORDER BY s.order_date DESC) AS rnk
   FROM sales AS s
   INNER JOIN members AS mem 
   ON s.customer_id = mem.customer_id AND s.order_date < mem.join_date
